7df9eddc3bc3f3602fc1939b108d1243cdee7a94
sebarun <runimas@gmail.com>
2026-06-16 19:47:37 +0200

    Replace logMsg with DBG for logging

--- power.cpp
+++ power.cpp
@@ -10,11 +10,11 @@ void powerInit() {
     pinMode(SIM7000_PWRKEY_PIN, OUTPUT);
     // Aseguramos el estado HIGH inicial (apoyando a tu resistencia de 10k)
     digitalWrite(SIM7000_PWRKEY_PIN, HIGH);
-    logMsg("Power init OK");
+    DBG("Power init OK");
 }
 
 void powerOnModem() {
-    logMsg("Encendiendo SIM7000G (Pin 33 LOW)...");
+    DBG("Encendiendo SIM7000G (Pin 33 LOW)...");
     
     // Pulso bajo para encender
     digitalWrite(SIM7000_PWRKEY_PIN, LOW);
@@ -25,11 +25,11 @@ void powerOnModem() {
     
     // Le damos tiempo al firmware del módem para arrancar y estar listo
     delay(3000); 
-    logMsg("SIM7000G Encendido.");
+    DBG("SIM7000G Encendido.");
 }
 
 void powerOffModem() {
-    logMsg("Apagando SIM7000G...");
+    DBG("Apagando SIM7000G...");
     
     // El apagado (Power Down) normal también requiere un pulso de >1.2s
     digitalWrite(SIM7000_PWRKEY_PIN, LOW);
@@ -38,11 +38,11 @@ void powerOffModem() {
     
     // Esperamos a que se apague de forma segura
     delay(2000);
-    logMsg("SIM7000G Apagado de forma segura.");
+    DBG("SIM7000G Apagado de forma segura.");
 }
 
 void enterDeepSleep() {
-    logMsg("Entrando en Deep Sleep...");
+    DBG("Entrando en Deep Sleep...");
     // NOTA: Actualmente lo tienes en 5 segundos (5000000 microsegundos).
     // Para un tracker real, esto suele ser mucho mayor (ej. 3 a 5 minutos).
     esp_sleep_enable_timer_wakeup(5000000); 
