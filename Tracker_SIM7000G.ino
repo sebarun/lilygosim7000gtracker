3840097d2830a05b9189a150e1796636fa3f0745
sebarun <runimas@gmail.com>
2026-06-16 15:05:54 +0200

    Update Tracker_SIM7000G.ino
    Nuevas funciones en tu flujo principal para que el módem se encienda al despertar y se apague antes de dormir para ahorrar el máximo de batería.

--- Tracker_SIM7000G.ino
+++ Tracker_SIM7000G.ino
@@ -9,12 +9,25 @@
 #include "battery.h"
 void setup(){
   Serial.begin(115200);
+  
+  // 1. Inicializar placa y periféricos
   loggerInit();
   storageInit();
   batteryInit();
-  logMsg("BOOT");
   powerInit();
+  
+  logMsg("=== BOOT TRACKER ===");
+  
+  // 2. Despertar el módem
+  powerOnModem();
+  
+  // 3. Ejecutar las tareas del rastreador
   handleSMS();
+  
+  // 4. Apagar el módem para no gastar batería durante el sueño
+  powerOffModem();
+
+  // 5. Dormir el ESP32
   enterDeepSleep();
 }
 void loop(){}
