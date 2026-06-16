2c75f3f807ebd1f0cd7aed3cb003eab0ce84f064
sebarun <runimas@gmail.com>
2026-06-16 18:12:36 +0200

    Delete gps.cpp
    duplicado

--- gps.cpp
+++
@@ -1,5 +0,0 @@
-#include "gps.h"
-#include "logger.h"
-bool getGPS(float&la,float&lo,float&sp){la=40;lo=-3;sp=0;return true;}
-void gpsEnable(){logMsg("GNSS ON");}
-void gpsDisable(){logMsg("GNSS OFF");}
