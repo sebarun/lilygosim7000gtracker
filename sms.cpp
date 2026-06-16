cd124a6594a991d0b44242733995f429d23a4416
sebarun <runimas@gmail.com>
2026-06-16 18:14:56 +0200

    Delete sms.cpp
    duplicado

--- sms.cpp
+++
@@ -1,4 +0,0 @@
-#include "sms.h"
-#include "gps.h"
-#include "logger.h"
-void handleSMS(){float la,lo,sp;gpsEnable();getGPS(la,lo,sp);logMsg(String("https://maps.google.com/?q=")+la+","+lo);gpsDisable();}
